bdea1e7416912be1d9dbda6fbc155620cae9fb49
Geovanny1029 <jose_32127@hotmail.com>
2019-09-24 17:09:54 -0500

    diseño css de index en autentificacion

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -42,6 +42,6 @@ class LoginController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('guest')->except('logout');
+         $this->middleware('guest', ['except' =>['logout','login'] ]);
     }
 }
